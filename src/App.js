99e2f04421e5fef4eff6c14a5fd25180147415cb
Imran Ansari <90140301+imran-7860@users.noreply.github.com>
2024-03-23 23:12:10 +0530

    added links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './screen/Home';
+import About from './screen/About';
+import Blog from './screen/Blog';
+import Destinations from './screen/Destinations';
+import FAQS from './screen/FAQS';
+import Hospitals from './screen/Hospitals';
+import Treatments from './screen/Treatments';
 
 
 function App() {
   return (
    <div>
-    <Home />
+    <Routes>
+      <Route path="/" element={ <Home />} />
+      <Route path="/about" element={ <About />} />
+      <Route path="/blog" element={ <Blog/>} />
+      <Route path="/destination" element={ <Destinations />} />
+      <Route path="/faq" element={ <FAQS/>} />
+      <Route path="/hospitals" element={ <Hospitals />} />
+      <Route path="/treatments" element={ <Treatments />} />
+     
+
+    </Routes>
+   
    </div>
    
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { CiSearch } from "react-icons/ci";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="bg-blue-500">
       <nav className="container mx-auto flex justify-between items-center  text-white  w-full">
         <div className="flex gap-6 text-lg font-medium">
-          <div className="ml-8">Home</div>
-          <div className="">About Us</div>
-          <div className="">Treatments +</div>
-          <div className="">Destinations +</div>
-          <div className="">Hospitals</div>
-          <div className="">FAQS</div>
-          <div className="">Blog</div>
+          <Link to="/" className="ml-8">Home</Link>
+          <Link to="/about" className="">About Us</Link>
+          <Link to="/treatments" className="">Treatments +</Link>
+          <Link to="/destination" className="">Destinations +</Link>
+          <Link to="/hospitals" className="">Hospitals</Link>
+          <Link to="/faq" className="">FAQS</Link>
+          <Link to="/blog" className="">Blog</Link>
           <div className="">Request A Quote</div>
         </div>
 
