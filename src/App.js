6751f03b34f06005589afcac9b657a0d5653aa25
Imran Ansari <90140301+imran-7860@users.noreply.github.com>
2024-03-29 01:07:50 +0530

    loader added

--- src/App.js
+++ src/App.js
@@ -1,32 +1,45 @@
-
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Home from './screen/Home';
-import About from './screen/About';
-import Blog from './screen/Blog';
-import Destinations from './screen/Destinations';
-import FAQS from './screen/FAQS';
-import Hospitals from './screen/Hospitals';
-import Treatments from './screen/Treatments';
-
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./screen/Home";
+import About from "./screen/About";
+import Blog from "./screen/Blog";
+import Destinations from "./screen/Destinations";
+import FAQS from "./screen/FAQS";
+import Hospitals from "./screen/Hospitals";
+import Treatments from "./screen/Treatments";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
-  return (
-   <div>
-    <Routes>
-      <Route path="/" element={ <Home />} />
-      <Route path="/about" element={ <About />} />
-      <Route path="/blog" element={ <Blog/>} />
-      <Route path="/destination" element={ <Destinations />} />
-      <Route path="/faq" element={ <FAQS/>} />
-      <Route path="/hospitals" element={ <Hospitals />} />
-      <Route path="/treatments" element={ <Treatments />} />
-     
+  const [loader, setLoader] = useState(true);
 
-    </Routes>
-   
-   </div>
-   
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoader(false)
+    }, 2000)
+  }, [])
+  return (
+    <div>
+      {loader ? (
+        <div className="h-screen flex justify-center items-center animate-pulse">
+          <img src={require("./assets/topsection/aafiya m.png")} alt=" " />
+
+        </div>
+      ) : (
+        <div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/destination" element={<Destinations />} />
+            <Route path="/faq" element={<FAQS />} />
+            <Route path="/hospitals" element={<Hospitals />} />
+            <Route path="/treatments" element={<Treatments />} />
+          </Routes>
+        </div>
+      )}
+    </div>
   );
 }
 
